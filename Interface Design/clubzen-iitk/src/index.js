f6a752a0d01e86c9f33a2de2811c9bdf00880d33
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-08 16:55:58 +0530

    Installed react router for routing system

--- Interface Design/clubzen-iitk/src/index.js
+++ Interface Design/clubzen-iitk/src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
