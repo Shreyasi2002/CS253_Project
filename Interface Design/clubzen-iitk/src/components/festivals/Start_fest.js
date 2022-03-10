de1ea09e45a69db81255068f88c493f539f0a0d5
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-10 11:13:39 +0530

    Added festival page

---
+++ Interface Design/clubzen-iitk/src/components/festivals/Start_fest.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import '../council_fest/Start.css'
+
+const StartFest = () => {
+  return (
+    <div className='council-start'>
+          <h1>FESTIVALS - IIT KANPUR</h1>
+          <p>
+              The four Councils - Science and Technology (SnT), Media
+              and Cultural (MnC), Games and Sports (GnS) and Academics
+              and Career (AnC) along with cells carry out the executive
+              responsibilities of the Students' Gymkhana.<br/>
+              Each council executes affairs pertaining to an overarching purpose.
+              <br/>
+              The Councils comprise Clubs, Hobby Groups, and Institute
+              Teams, which cater to specific activities.
+          </p>
+    </div>
+  )
+}
+
+export default StartFest
