683fee0cfb3d482075ae7064a9b50045c1763ab5
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-10 10:32:36 +0530

    Added responsiveness in navigation bar

--- Interface Design/clubzen-iitk/src/components/Navs.js
+++ Interface Design/clubzen-iitk/src/components/Navs.js
@@ -1,41 +1,73 @@
-import React from 'react'
-import { useLocation } from 'react-router';
+import React from "react";
+import { useLocation } from "react-router";
 
-import { NavLink } from './Links.styled'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-import './Navs.css'
+import { NavLink } from "./Links.styled";
+
+import "./Navs.css";
 
 const LINKS = [
-    { to: '/councils', text: 'Councils' },
-    { to: '/fests', text: 'Festivals' },
-    { to: '/clubs', text: 'Clubs'},
-    { to: '/news', text: 'News & Feed' },
-    { to: '/forum', text: 'Forum' },
+  { to: "/councils", text: "Councils" },
+  { to: "/fests", text: "Festivals" },
+  { to: "/clubs", text: "Clubs" },
+  { to: "/news", text: "News & Feed" },
+  { to: "/forum", text: "Forum" },
 ];
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+const overlay = (
+  <div id="myNav" className="overlay">
+    <p className="closebtn" onClick={closeNav}>
+      &times;
+    </p>
+    <ul className="overlay-content">
+      {LINKS.map((item) => (
+        <li key={item.to}>
+          <NavLink to={item.to}>{item.text}</NavLink>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 const Navs = () => {
   const location = useLocation();
   return (
+    <>
+      {overlay}
       <header>
-          <nav className='nav'>
-              <h3 className='nav-logo'><NavLink to="/">ClubZen IITK</NavLink></h3>
-              <ul className='nav-items'>
-                {LINKS.map(item => (
-                        <li key={item.to}>
-                            <NavLink
-                                to={item.to}
-                                className={
-                                    item.to === location.pathname ? 'active' : ''
-                                }
-                            >
-                                {item.text}
-                            </NavLink>
-                        </li>
-                    ))}
-              </ul>
-          </nav>
+        <nav className="nav">
+          <h3 className="nav-logo">
+            <NavLink to="/">ClubZen IITK</NavLink>
+          </h3>
+          <ul className="nav-items">
+            {LINKS.map((item) => (
+              <li key={item.to} className="menu-nav" id="navigation">
+                <NavLink
+                  to={item.to}
+                  className={item.to === location.pathname ? "active" : ""}
+                >
+                  {item.text}
+                </NavLink>
+              </li>
+            ))}
+            <li className="icon-menu">
+              <FontAwesomeIcon icon={faBars} onClick={openNav} />
+            </li>
+          </ul>
+        </nav>
       </header>
-  )
-}
+    </>
+  );
+};
 
-export default Navs
+export default Navs;
