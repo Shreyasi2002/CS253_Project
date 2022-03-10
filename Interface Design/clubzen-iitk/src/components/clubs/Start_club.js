f29abb9c94c3f0d87563af7ef607f3aa0446ad32
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-10 19:54:51 +0530

    Styling - responsive flexbox in home page

---
+++ Interface Design/clubzen-iitk/src/components/clubs/Start_club.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../council_fest/Start.css'
+
+const StartClub = () => {
+  return (
+    <div className='council-start'>
+          <h1>CLUBS & SOCIETIES - IIT KANPUR</h1>
+          <p>
+              An Opportunity to think outside the academic curriculum ...
+          </p>
+    </div>
+  )
+}
+
+export default StartClub
--- Interface Design/clubzen-iitk/src/pages/Clubs.js
+++ Interface Design/clubzen-iitk/src/pages/Clubs.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import StartClub from '../components/clubs/Start_club'
 
 import MainPageLayout from '../components/MainPageLayout'
 
 const Clubs = () => {
   return (
     <MainPageLayout>
-          
+          <StartClub />
     </MainPageLayout>
   )
 }
