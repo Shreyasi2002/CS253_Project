8062bb375f0a5db93d41979191fb574091d8f30b
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-15 23:16:44 +0530

    Created Calendar

--- Interface Design/clubzen-iitk/src/pages/Calendar.js
+++ Interface Design/clubzen-iitk/src/pages/Calendar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-// import CalendarShow from '../components/calendar/CalendarShow'
+import SearchEvents from '../components/calendar/CalendarShow'
+
 import Clock from '../components/calendar/Clock'
 import StartCalendar from '../components/calendar/StartCalendar'
 import MainPageLayout from '../components/MainPageLayout'
@@ -9,7 +10,7 @@ const Calendar = () => {
       <MainPageLayout >
           <StartCalendar />
           <Clock />
-          {/* <CalendarShow /> */}
+          <SearchEvents />
     </MainPageLayout>
   )
 }
