ab63f32b53c044438b37c3ed6264015477dadd4e
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-19 20:11:29 +0530

    Few modifications

--- Interface Design/clubzen-iitk/src/pages/Forum.js
+++ Interface Design/clubzen-iitk/src/pages/Forum.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Discussion from '../components/forum/Discussion'
 import StartForum from '../components/forum/StartForum'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Forum = () => {
   return (
       <MainPageLayout>
-         <StartForum /> 
+          <StartForum /> 
+          <Discussion />
       </MainPageLayout>
   )
 }
