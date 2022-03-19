17b47b0c662257292c0cf7b392efe5408707e1cb
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-19 19:07:50 +0530

    Added Forum page header

---
+++ Interface Design/clubzen-iitk/src/components/forum/StartForum.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../council_fest/Start.css'
+
+const StartForum = () => {
+  return (
+    <div className='council-start'>
+          <h1>JOIN THE TALK</h1>
+          <p>
+              Explore the forum below and find answers to all of your questions ...
+          </p>
+    </div>
+  )
+}
+
+export default StartForum
--- Interface Design/clubzen-iitk/src/pages/Forum.js
+++ Interface Design/clubzen-iitk/src/pages/Forum.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import StartForum from '../components/forum/StartForum'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Forum = () => {
   return (
       <MainPageLayout>
-          
+         <StartForum /> 
       </MainPageLayout>
   )
 }
