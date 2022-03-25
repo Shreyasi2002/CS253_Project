b7da9fd7d367d31561ff1b552d385e4e671e8504
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-25 14:08:47 +0530

    Club Individual pages

--- Interface Design/clubzen-iitk/src/pages/clubs/Astronomy.js
+++ Interface Design/clubzen-iitk/src/pages/clubs/Astronomy.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GalleryAstronomy from '../../components/clubs/astronomy/GalleryAstronomy'
 import StartHead from '../../components/clubs/astronomy/StartHead'
 import MainPageLayout from '../../components/MainPageLayout'
 
@@ -7,6 +8,7 @@ const Astronomy = () => {
   return (
       <MainPageLayout color='black'>
           <StartHead />
+          <GalleryAstronomy />
       </MainPageLayout>
   )
 }
