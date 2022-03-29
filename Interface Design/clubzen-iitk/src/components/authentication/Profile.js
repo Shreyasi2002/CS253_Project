3c77fc9f5652dc0a80a21b41a70f0c197ba1f02a
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-29 10:48:16 +0530

    Changes

--- Interface Design/clubzen-iitk/src/components/authentication/Profile.js
+++ Interface Design/clubzen-iitk/src/components/authentication/Profile.js
@@ -13,10 +13,6 @@ const ProfileDisplay = () => {
         </h3>
       </header>
       <p>
-        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-      </p>
-      <p>
         <strong>Id:</strong> {currentUser.id}
       </p>
       <p>
--- Interface Design/clubzen-iitk/src/pages/News.js
+++ Interface Design/clubzen-iitk/src/pages/News.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import StartNews from '../components/newsevents/StartNews'
 
 const News = () => {
   return (
       <MainPageLayout>
-          
+          <StartNews />
     </MainPageLayout>
   )
 }
