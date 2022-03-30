1de305f3e5667cc84a31772d4817cdbd666dce4c
Priyanka <priyankam8963@gmail.com>
2022-03-30 15:45:29 +0530

    three clubs pages done

---
+++ Interface Design/clubzen-iitk/src/components/clubs/electronics/StartHead.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './StartHead.css'
+
+const StartHead = () => {
+    return (
+      <>
+        <div className='container-h'>
+            <img src='/Images/clubs/electronics/background.jpg' alt='background' className='image-h' />
+            <div className='content-h'>
+                <h1>ELECTRONICS CLUB IITK</h1>
+                <p>For those who believes in innovation,imagination and application ...</p>
+            </div>
+        </div></>
+  )
+}
+
+export default StartHead
