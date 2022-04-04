dd465efc005e60fa9b33042e2296fd0588d55845
Avinash Prasad <76612206+avinashxx3@users.noreply.github.com>
2022-04-04 22:30:32 +0530

    updated clubzen unit

--- Backend Units/clubzen/src/main/java/com/clubzen/ClubzenApplication.java
+++ Backend Units/clubzen/src/main/java/com/clubzen/ClubzenApplication.java
@@ -2,13 +2,8 @@ package com.clubzen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.clubzen.controllers.clubzenController;
 
 @SpringBootApplication
-//@ComponentScan(basePackageClasses = clubzenController.class)
 public class ClubzenApplication {
 
 	public static void main(String[] args) {
